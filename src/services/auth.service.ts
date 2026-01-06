480bf440fe1458dc907107c6095bfcb426007f87
cyy <caiyiyang1998@126.com>
2026-01-06 15:42:42 +0800

    调试

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -168,6 +168,7 @@ export const authService = {
         if (!userString) {
             throw new AuthzUnauthorizedError('未登录')
         }
+        console.log(client)
         const userDto: UserDto = JSON.parse(userString)
         let userInExcludingList = false
         // if (client.extAttributes.userExcluding && client.extAttributes.userExcluding.includes(userDto.username)) {
