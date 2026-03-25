a98cb9b8c97df6a8764d8210f6af128d9758a539
cyy <cyy@shgas.com>
2026-03-25 16:30:14 +0800

    索引

--- src/routes/admin/admin.index.ts
+++ src/routes/admin/admin.index.ts
@@ -8,11 +8,11 @@ const router = createRouter();
 
 router.use("/*", authenicationHandler);
 
-router.openapi(routes.userInfo, handlers.userInfo)
-  .openapi(routes.passwordChange, handlers.passwordChange)
-  .openapi(routes.mobileSet, handlers.mobileSet)
-  .openapi(routes.organizationsSearch, handlers.organizationsSearch)
-  .openapi(routes.usersSearch, handlers.usersSearch)
-  .openapi(routes.usersQueryByOrg, handlers.usersQueryByOrg);
+// router.openapi(routes.userInfo, handlers.userInfo)
+//   .openapi(routes.passwordChange, handlers.passwordChange)
+//   .openapi(routes.mobileSet, handlers.mobileSet)
+//   .openapi(routes.organizationsSearch, handlers.organizationsSearch)
+//   .openapi(routes.usersSearch, handlers.usersSearch)
+//   .openapi(routes.usersQueryByOrg, handlers.usersQueryByOrg);
 
 export default router;
