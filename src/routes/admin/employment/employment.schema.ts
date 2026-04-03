f4a8b827aed6f69657a56048a268f0f06ea1d2de
cyy <cyy@shgas.com>
2026-04-03 10:00:12 +0800

    bug修复

--- src/routes/admin/employment/employment.schema.ts
+++ src/routes/admin/employment/employment.schema.ts
@@ -1,13 +1,13 @@
 import { z } from "@hono/zod-openapi";
 import { statusToString } from "@/enums/status";
-import { EmploymentDetailSchema, EmploymentDtoConverterSchema, EmploymentDtoSchema } from "@/services/employment/employment.schema";
+import { EmploymentDtoSchema } from "@/services/employment/employment.schema";
 
 export const EmploymentVoSchema = EmploymentDtoSchema.extend({
   statusText: z.string().openapi({ example: "正常" }),
 }).openapi("EmploymentVo");
 
-export const EmploymentVoConverterSchema = EmploymentDetailSchema.transform((e) => {
-  const dto = EmploymentDtoConverterSchema.parse(e);
+export const EmploymentVoConverterSchema = EmploymentDtoSchema.transform((dto) => {
+  // const dto = EmploymentDtoConverterSchema.parse(e);
   return {
     ...dto,
     statusText: statusToString[dto.status],
