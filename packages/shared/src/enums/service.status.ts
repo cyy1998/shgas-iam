19763045959eee7158030e6f7b5a587b895aef3f
cyy <cyy@shgas.com>
2026-04-17 11:44:33 +0800

    feat(shared): 初始化 @iam/shared 包，迁入 ServiceStatusCode 枚举

---
+++ packages/shared/src/enums/service.status.ts
@@ -0,0 +1,9 @@
+export enum ServiceStatusCode {
+  Success = 200,
+  Unauthorized = 401,
+  UserNotExisting = 4001,
+  WrongPassword = 4002,
+  Forbidden = 403,
+  Maintancing = 4031,
+  Failure = 99999,
+}
