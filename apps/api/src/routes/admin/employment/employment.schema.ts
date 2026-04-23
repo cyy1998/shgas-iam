5145f165ef1dbb939cbf19f7413bbf2d3573dbc9
cyy <cyy@shgas.com>
2026-04-23 09:59:15 +0800

    feat(api): employment VO 增加 DetailVo

--- apps/api/src/routes/admin/employment/employment.schema.ts
+++ apps/api/src/routes/admin/employment/employment.schema.ts
@@ -1,15 +1,25 @@
 import { z } from "@hono/zod-openapi";
 import { statusToString } from "@/enums/status";
-import { EmploymentDtoSchema } from "@/services/employment/employment.schema";
+import { EmploymentDetailDtoSchema, EmploymentDtoSchema } from "@/services/employment/employment.schema";
 
 export const EmploymentVoSchema = EmploymentDtoSchema.extend({
   statusText: z.string().openapi({ example: "正常" }),
 }).openapi("EmploymentVo");
 
-export const EmploymentVoConverterSchema = EmploymentDtoSchema.transform((dto) => {
-  // const dto = EmploymentDtoConverterSchema.parse(e);
-  return {
-    ...dto,
-    statusText: statusToString[dto.status],
-  };
-}).pipe(EmploymentVoSchema);
+export const EmploymentVoConverterSchema = EmploymentDtoSchema.transform(dto => ({
+  ...dto,
+  statusText: statusToString[dto.status],
+})).pipe(EmploymentVoSchema);
+
+export const EmploymentDetailVoSchema = EmploymentDetailDtoSchema.extend({
+  statusText: z.string().openapi({ example: "正常" }),
+  privileges: z.array(z.string()).openapi({ example: ["ui:button:tender:create-GYBG"] }),
+  roles: z.array(z.string()).openapi({ example: ["tender:default-user"] }),
+}).openapi("EmploymentDetailVo");
+
+export const EmploymentDetailVoConverterSchema = EmploymentDetailDtoSchema.transform(dto => ({
+  ...dto,
+  privileges: dto.privileges ?? [],
+  roles: dto.roles ?? [],
+  statusText: statusToString[dto.status],
+})).pipe(EmploymentDetailVoSchema);
