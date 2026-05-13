a8958625cb0c5f038250bd5247ca50d350041f95
cyy1998 <caiyiyang1998@126.com>
2026-05-13 10:25:51 +0800

    chore(fix): 管理端api静态文件

--- apps/admin-api/src/env.ts
+++ apps/admin-api/src/env.ts
@@ -9,7 +9,7 @@ const EnvSchema = z.object({
   REDIS_PASSWORD: z.string().optional().transform(value => value || undefined),
   REDIS_DB: z.coerce.number(),
   LOG_LEVEL: z.string().default("info"),
-  ADMIN_CLIENT_CODES: z.string().default("iam"),
+  ADMIN_CLIENT_CODES: z.string().default("iam-admin"),
   ADMIN_ROLE_CODES: z.string().default("iam:admin"),
 });
 
